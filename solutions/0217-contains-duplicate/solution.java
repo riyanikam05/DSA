6e2a6aef21dfd48d390115453572d8a9311ab477
riyanikam05 <riyanikam005@gmail.com>
2026-06-17 10:11:41 +0000

    Sync LeetCode submission Runtime - 14 ms (89.77%), Memory - 93.5 MB (52.37%)

--- solutions/0217-contains-duplicate/solution.java
+++ solutions/0217-contains-duplicate/solution.java
@@ -1,10 +1,25 @@
-class Solution {
+//Brute Force
+/**class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
         for(int i = 0; i < n-1; i++){
             if (nums[i] == nums[i+1])
                 return true;
+        }
+        return false;
+    }
+} */
+
+//Optimized : HashSet (TC = O(n))
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums){
+            if (seen.contains(num))
+                return true;
+            seen.add(num);    
         }
         return false;
     }
