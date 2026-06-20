e6910d8e0a8872dd8416230459276fdd209375f5
riyanikam05 <riyanikam005@gmail.com>
2026-06-20 11:19:38 +0000

    Sync LeetCode submission Runtime - 1 ms (62.67%), Memory - 43.3 MB (32.82%)

--- solutions/0014-longest-common-prefix/solution.java
+++ solutions/0014-longest-common-prefix/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
+        if (strs[0] == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {  //Store 1st string (i.e. flower) in ch
+            char ch = strs[0].charAt(i);
 
-        String pref = strs[0];
-        int prefLen = pref.length();
-
-        for (int i = 0; i < strs.length; i++) {
-            String s = strs[i];
-            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
-                prefLen--;
-                if (prefLen ==0) {
-                    return "";
-                }
-                pref = pref.substring(0, prefLen);
+        for (int j = 1; j < strs.length; j++) {   //Compare the remaining strings (i.e. flow & flight) with 1st string
+            if (i >= strs[j].length() || strs[j].charAt(i) != ch) {  //if String ended or Characters don't match, Return prefix found so far.
+                return strs[0].substring(0, i);
             }
         }
-
-        return pref;
+        }
+        return strs[0];
     }
 }
