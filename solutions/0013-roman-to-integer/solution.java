16d1e15715845fe5811503ff895615cc4b0d1f8e
riyanikam05 <riyanikam005@gmail.com>
2026-06-20 11:57:46 +0000

    Sync LeetCode submission Runtime - 5 ms (33.44%), Memory - 47 MB (14.36%)

--- solutions/0013-roman-to-integer/solution.java
+++ solutions/0013-roman-to-integer/solution.java
@@ -10,15 +10,15 @@ class Solution {
         roman.put('D', 500);
         roman.put('M', 1000);
 
-        for(int i = 0; i < s.length() - 1; i++){
-            if(roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))){
+        for(int i = 0; i < s.length() - 1; i++){  //stop at the 2nd last character to compare s.charAt(i) and s.chyarAt(i+1)
+        if(roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))){  //if i < i+1 -> s.charAt(i+1)-s.charAt(i)
                 res -= roman.get(s.charAt(i));
             }
             else {
-                res += roman.get(s.charAt(i));
+                res += roman.get(s.charAt(i));   //else add.
             }
         }       
 
-        return res + roman.get(s.charAt(s.length() - 1));
+        return res + roman.get(s.charAt(s.length() - 1));  //The loop never processes the last character, so we add it separately.
         }
 }
