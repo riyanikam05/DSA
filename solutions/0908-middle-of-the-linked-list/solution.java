19067ee272bc7547985d1bc65cd9e80898214822
riyanikam05 <riyanikam005@gmail.com>
2026-06-27 09:43:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (86.08%)

---
+++ solutions/0908-middle-of-the-linked-list/solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) return head;  //if list is empty or has only 1 node (i.e. head), return head (the single node oe empty).
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
